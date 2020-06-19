3e6355f7a7b1ab7301b34d04f3c67faf53f453bc
Jae Yoo <pppjqqq@gmail.com>
2020-06-19 12:09:16 -0700

    seed data

---
+++ server/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+const express = require('express');
+// const Reviews = require('../database/reviews.js');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./client/dist'));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
